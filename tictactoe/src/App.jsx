01f3e6aadfe07d65ce458d93512e7c46dfd0ac03
unknown <dasguptadipayan8.com>
2022-06-29 19:04:02 +0530

    init commit

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const app = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this tictactoe app.</p>
+    </>
+  );
+};
+export default app;
